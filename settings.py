c023d0d4f45e8e7ab6088ac58d6a9c4d4c09614e
sinnermor-fox <akordyukova.pronto@gmail.com>
2021-03-18 22:36:01 +0300

    first menu bot version only dayly precounted

---
+++ settings.py
@@ -0,0 +1,9 @@
+
+TG_TOKEN = "1604584263:AAHO8HlucPWG8JkLH87nTGGwlw337U1UdRk"
+PROXY = {
+    'proxy_url': 'socks5://t1.learn.python.ru:1080',
+    'urllib3_proxy_kwargs': {
+        'username': 'learn',
+        'password': 'python'
+    }
+}
